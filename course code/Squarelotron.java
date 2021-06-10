007dbf350a0f092d793d659c7e87b5c2ac45fac8
JasonZhangHub <jszhang0001@gmail.com>
2021-06-10 12:37:56 +0800

    Update Squarelotron.java

--- course code/Squarelotron.java
+++ course code/Squarelotron.java
@@ -1,67 +1,79 @@
 import java.util.*;
 
 public class Squarelotron {
-	int size; //size of the squarelotron
 	int[][] squarelotron;
-
-	//constructor
-	public Squarelotron(int n){
+	int size;
+	
+	/*
+	 * Fills the 2-dimensional array with the numbers 1 to n squared, in order. 
+	 * Sets the size instance variable to be n.
+	 */
+	public Squarelotron(int n) {
 		this.size = n;
-		int count = 0;
+		int value = 1;
 		squarelotron = new int[n][n];
-		for(int i=0;i<n;i++) {
-			for(int j=0; j<n;j++) {
-				count++;
-				squarelotron[i][j] = count;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				this.squarelotron[i][j] = value;
+				value++;
 			}
 		}
-		
 	}
 	
-	//method:upsideDownFlip
+	/*
+	 * checks whether a square is located inside the provided ring
+	 */
+	public boolean isOnRing(int i, int j, int ring) {
+		boolean isOn = (i == ring - 1 || j == ring - 1 
+				|| i == size - ring || j == size - ring); // a square has at least 1 side on the ring
+		boolean isOut = (i <= ring - 2 || i >= size + 1 - ring 
+				|| j <= ring - 2 || j >= size + 1 - ring); // the square is outside the ring
+		return isOn && !isOut; // on or not outside => inside
+	}
+	
+	/*
+	 *  performs the Upside-Down Flip of the squarelotron 
+	 *  returns the new squarelotron. 
+	 *  The original squarelotron should not be modified.
+	 */
 	public Squarelotron upsideDownFlip(int ring) {
-		Squarelotron s = new Squarelotron(this.size);
-		Squarelotron s_new = new Squarelotron(this.size);
-
-		for(int i =0; i<this.size; i++) {
-			for(int j=0; j<this.size; j++) {
-				if(((i == ring - 1) || (i == this.size -ring) || (j== ring -1) || (j==this.size-ring))
-					&& (i<= this.size-ring) && (i>=ring-1) && (j<= this.size - ring) && (j>=ring-1)
-					) {
-					s_new.squarelotron[i][j] = s.squarelotron[this.size- 1 - i][j];
+		Squarelotron newSquarelotron = new Squarelotron(size);
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				if (isOnRing(i, j, ring)) {
+					newSquarelotron.squarelotron[i][j] = this.squarelotron[size - 1 - i][j]; // flip pattern
 				}
-				
 			}
 		}
+		return newSquarelotron;
 		
-		return s_new;
 	}
 	
-	//method:mainDiagonalFlip
+	/*
+	 *  performs the  Main Diagonal Flip of the squarelotron 
+	 *  returns the new squarelotron. 
+	 *  The original squarelotron should not be modified.
+	 */
 	public Squarelotron mainDiagonalFlip(int ring) {
-		Squarelotron s = new Squarelotron(this.size);
-		Squarelotron s_new = new Squarelotron(this.size);
-
-		for(int i =0; i<this.size; i++) {
-			for(int j=0; j<this.size; j++) {
-				if(((i == ring - 1) || (i == this.size -ring) || (j== ring -1) || (j==this.size-ring))
-					&& (i<= this.size-ring) && (i>=ring-1) && (j<= this.size - ring) && (j>=ring-1)
-					) {
-					s_new.squarelotron[i][j] = s.squarelotron[j][i];
+		Squarelotron newSquarelotron = new Squarelotron(size);
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				if (isOnRing(i, j, ring)) {
+					newSquarelotron.squarelotron[i][j] = this.squarelotron[j][i]; // flip pattern
 				}
-				
 			}
 		}
-		
-		return s_new;
+		return newSquarelotron;
 	}
-
-	//method:rotateRight 
-	//there is seasonal in the rotation. rotateRight(-1) = rotateRight(3)
-	public void rotateRight(int numberOfTurns ) {
-		
-		numberOfTurns = (((numberOfTurns % 4) + 4 ) % 4 );
-		
+	
+	/*
+	 * The argument numberOfTurns indicates the number of times the entire squarelotron should be rotated 90° clockwise. 
+	 * Any integer, including zero and negative integers, is allowable as the argument. 
+	 * A value of -1 indicates a 90° counterclockwise rotation. 
+	 * This method modifies the internal representation of the squarelotron; it does not create a new squarelotron.
+	 */
+	public void rotateRight(int numberOfTurns) {
+		numberOfTurns = (numberOfTurns % 4 + 4) % 4; // take care of negative number of turns
 		for (int n = 0; n < numberOfTurns; n++) {
 			Squarelotron newSquarelotron = new Squarelotron(size);
 			for (int i = 0; i < size; i++) {
@@ -71,19 +83,18 @@ public class Squarelotron {
 			}
 			this.squarelotron = newSquarelotron.squarelotron;
 		}
-		return;
-		}
-	
+	}
 
 	public static void main(String[] args) {
-		
-		Squarelotron s = new Squarelotron(5);
-		s.rotateRight(1);
-		for (int i = 0; i < 5; i++) {
-			for (int j =0; j<5; j++) {
-				System.out.print(s.squarelotron[i][j] + "    ");
+		int n = 5;
+		Squarelotron se = new Squarelotron(n);
+		se = se.mainDiagonalFlip(3);
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print(se.isOnRing(i, j, 3));
 			}
-			System.out.print(" \n");
+			System.out.println();
 		}
 	}
+	
 }
